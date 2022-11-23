82709e8a512ad926c3d2b778c6d531944a94b5eb
Ildar <ildar.azmatov@ya.ru>
2022-11-23 16:55:33 +0500

    add Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 3;
+int b = 1;
+int c = 6;
+int d = 2;
+int e = 8;
+
+int max = a;
+
+if (a>max) max = a;
+if(b>max) max = b;
+if(c>max) max = c;
+if(d>max) max = d;
+if(e>max) max = e;
+
+Console.Write("max= ");
+Console.WriteLine(max);
