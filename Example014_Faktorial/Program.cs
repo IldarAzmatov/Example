5f9955d7722c1385efd3d8119e37f027915433b3
Ildar <ildar.azmatov@ya.ru>
2022-12-08 17:51:41 +0500

    Факториал числа

---
+++ Example014_Faktorial/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// int Factorial(int n)
+// {
+//     if (n == 0) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// Console.WriteLine(Factorial(3)); //3*2*1
+
+double Factorial(int n)
+{
+    if (n == 0) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i<40; i++)
+{
+Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
